6f3eda43a7ef858d7925f8ff74832b50cdc5146d
sonamr <sonamrastogi8@gmail>
2023-01-25 18:36:28 +0530

    25th first commit

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 class BlogData{
-    constructor(fname,lname,title,content){
-        this.fname = fname;
-        this.lname = lname;
-        this.title = title;
-        this.content = content;
-    }
+  constructor(fname,lname,title,content){
+      this.fname = fname;
+      this.lname = lname;
+      this.title = title;
+      this.content = content;
+  }
 }
 All_Data = new Array();    
 function submitData(){
 
-    alert("Thank you for submitting a blog. We will review it and upload it")
-    
-    let fname = document.forms["myForm"]["fname"].value;
-    let lname = document.forms["myForm"]["lname"].value;
-    let title = document.forms["myForm"]["title"].value;
-    let content = document.forms["myForm"]["content"].value;
-    
-    
+  alert("Thank you! for submitting your blog.")
+  
+  let fname = document.forms["myForm"]["fname"].value;
+  let lname = document.forms["myForm"]["lname"].value;
+  let title = document.forms["myForm"]["title"].value;
+  let content = document.forms["myForm"]["content"].value;
+  
+  
 
-    const obj = new BlogData(fname,lname,title,content);
-   
-    All_Data.push(obj);
-    console.log(All_Data);
-   
-   document.forms["myForm"]["fname"].value='';
-  document.forms["myForm"]["lname"].value='';
-     document.forms["myForm"]["title"].value='';
-    document.forms["myForm"]["content"].value='';
-    
-    return false;
+  const obj = new BlogData(fname,lname,title,content);
+ 
+  All_Data.push(obj);
+  console.log(All_Data);
+ 
+ document.forms["myForm"]["fname"].value='';
+document.forms["myForm"]["lname"].value='';
+   document.forms["myForm"]["title"].value='';
+  document.forms["myForm"]["content"].value='';
+  
+  return false;
 
 }
+
 const myModal = document.getElementById('myModal')
 const myInput = document.getElementById('myInput')
 
@@ -43,3 +44,34 @@ const myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
+
+//modal//
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+
+// Login Alert//
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('Thanks ! For the Signup')
+  })
+}
